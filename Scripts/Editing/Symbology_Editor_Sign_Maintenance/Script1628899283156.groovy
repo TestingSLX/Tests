a0cb644040f5afe6fdfbaef2dd6dfc768015a3a5
shrut <shrut@DESKTOP-FRI2B4N>
2021-08-13 20:20:20 -0400

    Symbology Editor

--- Scripts/Editing/Symbology_Editor_Sign_Maintenance/Script1628899283156.groovy
+++ Scripts/Editing/Symbology_Editor_Sign_Maintenance/Script1628899283156.groovy
@@ -27,51 +27,69 @@ WebUI.click(findTestObject('Editing/Symbology_Editor_Sign_Maintenance/Page_Stree
 
 WebUI.click(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/li_Sign Maintenance'))
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/h1_Symbology Editor'), 'Symbology Editor')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/h1_Symbology Editor'), 
+    'Symbology Editor')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/label_Select asset'), 'Select asset')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/label_Select asset'), 
+    'Select asset')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Label'), 'Label')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Label'), 
+    'Label')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Symbol'), 'Symbol')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Symbol'), 
+    'Symbol')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Count'), 'Count')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Count'), 
+    'Count')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Replace'), 'Replace')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Replace'), 
+    'Replace')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Retire'), 'Retire')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Retire'), 
+    'Retire')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Do Nothing'), 'Do Nothing')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Do Nothing'), 
+    'Do Nothing')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Defer'), 'Defer')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Defer'), 
+    'Defer')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Replace - Critical'), 'Replace - Critical')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/th_Replace - Critical'), 
+    'Replace - Critical')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/p_Transparency'), 'Transparency:')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/p_Transparency'), 
+    'Transparency:')
 
 WebUI.click(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Transparency050100'))
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/h3_Sign Maintenance'), 'Sign Maintenance')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/h3_Sign Maintenance'), 
+    'Sign Maintenance')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Maintenance Suggestion'), 'Maintenance Suggestion')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Maintenance Suggestion'), 
+    'Maintenance Suggestion')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Replace'), 'Replace')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Replace'), 
+    'Replace')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Retire'), 'Retire')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Retire'), 
+    'Retire')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Do Nothing'), 'Do Nothing')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Do Nothing'), 
+    'Do Nothing')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Defer'), 'Defer')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Defer'), 
+    'Defer')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Replace - Critical'), 'Replace - Critical')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Replace - Critical'), 
+    'Replace - Critical')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_others'), 'others')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_others'), 
+    'others')
 
 WebUI.click(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Esri_sc-gKAaRy bVZRMQ'))
 
-WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/button_Sign Maintenance'), 'SIGN MAINTENANCE')
+WebUI.verifyElementText(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/button_Sign Maintenance'), 
+    'SIGN MAINTENANCE')
 
 WebUI.click(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Sign Maintenance (Features 21,844, Selected 0)'))
-
-WebUI.click(findTestObject('Object Repository/Editing/Symbology_Editor_Sign_Maintenance/Page_Streetlogix/div_Esri_sc-gKAaRy bVZRMQ'))
 
