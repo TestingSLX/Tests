637b18085267eec6fd396eb7e6e37afb98f6bb20
Shrutika Sawant <shrutikasawant42@gmail.com>
2021-12-17 16:36:05 -0500

    Changing tests in test suites

--- Scripts/New Test Case/Script1632344918328.groovy
+++ Scripts/New Test Case/Script1632344918328.groovy
@@ -5,4 +5,16 @@ import java.sql.Statement
 
 import groovy.json.JsonSlurper
 
+Connection c = null
+c = DriverManager.getConnection("jdbc:postgresql://castreetlogix.ckjgcig5seif.ca-central-1.rds.amazonaws.com/development_portal", "sde", "V0ters!23");
+def queryString = "SELECT * from sde.pavement_evaluation_loc where ward is null"
+Statement stm = c.createStatement()
+ResultSet result = stm.executeQuery(queryString)
+double PCI = 0;
+double totalLength = 0;
+double pciLength = 0;
 
+	PCI = result.getInt('pci')
+	length = result.getInt('length_ft')
+	pciLength = pciLength + PCI * length
+	totalLength = totalLength + leng
