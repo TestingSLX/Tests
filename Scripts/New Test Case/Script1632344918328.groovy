5ec909fd88432d56f72f44e789eff1179772a4ef
Shrutika Sawant <shrutikasawant42@gmail.com>
2021-12-17 16:34:50 -0500

    CHanging Test suite tests

--- Scripts/New Test Case/Script1632344918328.groovy
+++ Scripts/New Test Case/Script1632344918328.groovy
@@ -5,12 +5,4 @@ import java.sql.Statement
 
 import groovy.json.JsonSlurper
 
-Connection c = null
-c = DriverManager.getConnection("jdbc:postgresql://castreetlogix.ckjgcig5seif.ca-central-1.rds.amazonaws.com/development_portal", "sde", "V0ters!23");
-def queryString = "SELECT * from sde.pavement_evaluation_loc where ward is null"
-Statement stm = c.createStatement()
-ResultSet result = stm.executeQuery(queryString)
-double PCI = 0;
-double totalLength = 0;
-double pciLength = 0;
 
